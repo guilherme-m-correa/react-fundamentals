8ebfeedba28307c6e8e7c61c62242d57a393b233
Guilherme Corrêa <guilherme.matheus.correa@gmail.com>
2023-12-26 20:45:32 -0300

    feat: Add header logo and more styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
 import styles from './Header.module.css';
+import igniteLogo from '../assets/ignite-logo.svg';
 
 export function Header() {
   return (
     <header className={styles.header}>
+     <div>
+      <img src={igniteLogo} alt="Ignite" />
       <strong>Ignite Feed</strong>
+     </div>
     </header>
   );
 }
