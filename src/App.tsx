3a0e9480400f2b8c75da504fbe676560c1815239
Guilherme Corrêa <guilherme.matheus.correa@gmail.com>
2023-12-26 18:25:18 -0300

    feat: Add Header with CSS Modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import Post from "./Post"
+import { Header } from "./components/Header"
+import Post from "./components/Post"
 
 function App() {
   return (
     <div>
+      <Header />
       <Post 
         author="John Doe"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus."
