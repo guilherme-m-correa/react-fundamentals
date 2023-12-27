2e4b7d8d2ff47513ebd39db31f131badea3488f0
Guilherme Corrêa <guilherme.matheus.correa@gmail.com>
2023-12-26 21:16:08 -0300

    feat: Add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
+import styles from './App.module.css'
 import { Header } from "./components/Header"
 import Post from "./components/Post"
-
 import './global.css'
 
 function App() {
   return (
     <div>
       <Header />
-      <Post 
-        author="John Doe"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus."
-        />
-      <Post
-        author="Jane Doe"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus."
-      />
+      <div className={styles.wrapper}>
+        <aside>
+          Sidebar
+        </aside>
+        <main>
+          <Post
+            author="John Doe"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus."
+          />
+          <Post
+            author="Jane Doe"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus."
+          />
+        </main>
+      </div>
     </div>
   )
 }
