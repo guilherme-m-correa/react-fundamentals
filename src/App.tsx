dbef4c38c36cea9c7a8ba9734522667fd4835d55
Guilherme Corrêa <guilherme.matheus.correa@gmail.com>
2024-01-14 22:32:53 -0300

    refactor: Extend interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import styles from './App.module.css'
 import { Header } from "./components/Header"
-import { Post } from "./components/Post"
+import { Post, PostType } from "./components/Post"
 import { Sidebar } from './components/Sidebar'
 import './global.css'
 
-const posts = [
+const posts: PostType[] = [
   {
     id: 1,
     author: {
@@ -46,9 +46,7 @@ function App() {
             return (
               <Post
                 key={post.id}
-                author={post.author}
-                content={post.content}
-                publishedAt={post.publishedAt}
+                post={post}
               />
             )
           })}
